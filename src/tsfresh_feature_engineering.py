0faec7adc640e127e2c23fd24439f9a9e02f88a0
kandersonko <kandersonko@gmail.com>
2023-11-30 22:05:43 -0800

    Fix tsfresh feature extraction with pivot

--- src/tsfresh_feature_engineering.py
+++ src/tsfresh_feature_engineering.py
@@ -20,7 +20,7 @@ from tsfresh.feature_extraction import feature_calculators, settings
 from tsfresh.utilities.distribution import ClusterDaskDistributor
 from tsfresh.convenience.bindings import dask_feature_extraction_on_chunk
 
-from tsfresh.utilities.dataframe_functions import impute
+from tsfresh.utilities.dataframe_functions import impute, impute_dataframe_zero
 
 import dask
 from dask.distributed import Client
@@ -66,34 +66,48 @@ def process_chunk(chunk_x, cluster, client, chunk_id, default_fc_parameters):
     time_array = np.tile(np.arange(chunk_x.shape[1]), chunk_x.shape[0])
     df_chunk = pd.DataFrame({'id': ids, 'time': time_array, 'value': chunk_x.flatten()})
 
+    # workers_info = client.scheduler_info()["workers"]
+    # n_workers = len(workers_info)
+    # n_cores = None
+    # if n_workers == 0:
+    #     n_cores = 4
+    #     n_workers = 1
+    # else:
+    #     n_cores = int([workers_info[w]["metrics"]["cpu"] for w in workers_info][0])
+    # n_jobs = n_workers * n_cores
+
+    # chunk_size, extra = divmod(chunk_x.shape[0], n_workers * 5)
+    # if extra:
+    #     chunk_size += 1
+
+    # print("n_cores: ", n_cores)
+    # print(f"n_workers: {n_workers}")
+    # print(f"n_jobs: {n_jobs}")
+    # print(f"chunk_size: {chunk_size}")
+
     # convert chunk to dask dataframe
-    data_chunk = dd.from_pandas(df_chunk, npartitions=5)  # adjust npartitions as needed
+    # data_chunk = dd.from_pandas(df_chunk, chunksize=chunk_size)
+    n_jobs = 4
+    data_chunk = dd.from_pandas(df_chunk, npartitions=10)
 
     # extract features using tsfresh with dask for the chunk
 
-    distributor = ClusterDaskDistributor(cluster)
+    # distributor = ClusterDaskDistributor(cluster)
+
+    # compute the number of n_jobs based on the number of workers
 
     chunk_features = extract_features(
-        data_chunk.compute(),
+        data_chunk,
         column_id="id",
         column_sort="time",
         column_value="value",
-        distributor=distributor,
+        # distributor=distributor,
         default_fc_parameters=default_fc_parameters,
         disable_progressbar=True,
-        pivot=False,
-        n_jobs=4,
+        # pivot=False,
+        n_jobs=n_jobs, # default to all cores
+        # impute_function=impute,
     )
-
-    # df_grouped = data_chunk.groupby(["id", "kind"])
-    # chunk_features = dask_feature_extraction_on_chunk(
-    #     df_grouped,
-    #     column_id="id",
-    #     column_kind=target_label,
-    #     column_sort="time",
-    #     column_value="value",
-    #     default_fc_parameters=settings.efficientfcparameters(),
-    # )
 
     return chunk_features
 
@@ -105,54 +119,49 @@ def feature_engineering(data_root_dir, target_label, subset, client, cluster):
     dataset_path = data_root_dir+f'{target_label}_dataset.npz'
     dataset = np.load(dataset_path, allow_pickle=True)
 
-    X = dataset[f'X_{subset}']
-    y = dataset[f'y_{subset}']
+    X = dataset[f'X_{subset}'][:100]
+    y = dataset[f'y_{subset}'][:100]
+
 
     print("X shape: ", X.shape)
     print("y shape: ", y.shape)
 
-    print("Format the data for tsfresh")
+    # scale the dask workers
+    cluster.scale(10)
 
     plot_data = []
 
-
     # distributor = ClusterDaskDistributor(cluster)
-
 
     print("Extracting features")
 
-    # Determine chunk size
-    chunk_size = 1000
-
-    # Initialize empty list to collect features from each chunk
-    chunk_features_list = []
-
     start_time = time.time()
+
+    chunk_size = 10
+
+    chunk_features_list = []
 
     # Process each chunk
     for chunk_start in tqdm(range(0, len(X), chunk_size)):
         chunk_X = X[chunk_start:chunk_start + chunk_size]
+
         # scatter the chunk to the workers
         # chunk_X = client.scatter(chunk_X, broadcast=True)
-        chunk_features = process_chunk(chunk_X, cluster, client, chunk_start, settings.EfficientFCParameters())
+        default_fc_parameters = settings.EfficientFCParameters()
+        chunk_features = process_chunk(chunk_X, cluster, client, chunk_start, default_fc_parameters)
         chunk_features_list.append(chunk_features)
 
-    # Compute all tasks in parallel
-    chunk_features_list = compute(*delayed_tasks)
 
+    chunk_features_list = dask.compute(*chunk_features_list)
     # Combine features from all chunks
-    combined_features = pd.concat(chunk_features_list)
+    features = pd.concat(chunk_features_list, axis=0)
 
-    features = impute(combined_features)
+    # print("features extracted: ", len(features['value']))
+    print("imputing features")
+    features = impute(features)
 
-    # select relevant features
-
-    print("len features: ", len(features))
-    print("len y: ", len(y))
-    print("features head: ", features.head())
-    # transform the labels to numpy arrays
-    # features_filtered = select_features(features, y)
-    # features_filtered = features_filtered.compute()
+    # print("selecting features")
+    # features = select_features(features, y)
 
     end_time = time.time()
     duration = end_time - start_time
@@ -162,15 +171,9 @@ def feature_engineering(data_root_dir, target_label, subset, client, cluster):
     plot_data = pd.DataFrame(plot_data)
     plot_data.to_csv(f'data/tsfresh_plot_data_{target_label}_{subset}.csv')
 
-    features[target_label] = y
+    # save the features with the labels
+    # features[target_label] = y
     features.to_csv(data_root_dir+f'{target_label}_{subset}_tsfresh.csv')
-
-    # print("filtered features")
-    # print(features_filtered.head())
-
-    # save the features with the labels concatenated
-    # features_filtered[target_label] = y
-    # features_filtered.to_csv(data_root_dir+f'{target_label}_{subset}_tsfresh.csv')
 
 
 def main():
@@ -184,13 +187,13 @@ def main():
     cluster = SLURMCluster(
         cores=16,
         processes=4,
-        memory="8GB",
+        memory="16GB",
         walltime="00:30:00",
         shebang="#!/usr/bin/env sh",
         job_script_prologue=job_script_prologue,
         worker_extra_args=["--lifetime", "25m", "--lifetime-stagger", "4m"],
-        # interface='ib0',
-        queue="short",
+        interface='ib0',
+        queue="eight",
         # death_timeout=120,
         log_directory="jobs/dask_logs",
         # local_directory="/tmp/",
